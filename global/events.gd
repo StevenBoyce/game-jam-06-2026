6d4bdb7af1e119ffe8ce8fd0ab08c6ea8528a1d2
StevenBoyce <steven.boyce.br@gmail.com>
2026-06-14 15:42:18 -0600

    Initial structure

---
+++ global/events.gd
@@ -0,0 +1,3 @@
+extends Node
+
+signal an_event
---
+++ resource_types/mission.gd
@@ -0,0 +1,8 @@
+class_name Mission
+extends Resource
+
+@export var name: String
+@export var description: String
+@export var rewards: String # TODO: Add rewards type
+# @export var rewards: Array[Reward]
+@export var difficulty: int
